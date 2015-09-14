3b634e747106121ebeb9fdf958234790f902a425
mcpaul19 <pbm12@pitt.edu>
2015-09-14 11:28:34 -0400

    Added test

---
+++ test.java
@@ -0,0 +1,13 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("She take my money, when I'm in need");
+		System.out.println("Aww she a gold digger, friend indeed");
+
+
+	}
+
+}
